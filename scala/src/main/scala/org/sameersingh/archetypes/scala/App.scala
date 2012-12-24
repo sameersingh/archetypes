460dff7f09b89779fa48847595bd71ef997d3198
Sameer Singh <sameeersingh@gmail.com>
2012-12-23 19:09:09 -0500

    initial scala archetype

---
+++ scala/src/main/scala/org/sameersingh/archetypes/scala/App.scala
@@ -0,0 +1,12 @@
+package org.sameersingh.archetypes.scala
+
+/**
+ * @author sameer
+ * @date 10/30/12
+ */
+object App {
+  def main(args:Array[String]) = {
+    println("Hello World");
+  }
+}
+
---
+++ scala/src/test/scala/org/sameersingh/archetypes/scala/AppTest.scala
@@ -0,0 +1,17 @@
+package org.sameersingh.archetypes.scala
+
+import org.junit._
+
+/**
+ * @author sameer
+ * @date 10/30/12
+ */
+@Test
+class PlotTest {
+
+  @Test
+  def testApp(): Unit = {
+    App.main(Seq.empty[String].toArray)
+  }
+
+}
